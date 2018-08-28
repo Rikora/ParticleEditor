ee7779f8799591f149702b5c5382ca66b9bf8da4
JAKOB\Jakob <jakob.ahrneteg@gmail.com>
2018-08-28 17:33:59 +0200

    Added some intitial values for particles and a few editing operations

--- ParticleEditor/src/editor/Application.cpp
+++ ParticleEditor/src/editor/Application.cpp
@@ -18,13 +18,7 @@ namespace px
 		// Load texture
 		m_particleTexture.loadFromFile("src/res/textures/particle.png");
 		m_particleSystem.setTexture(m_particleTexture);
-
-		// Prepare the particle emitter
-		m_emitter.setEmissionRate(10);
-		m_emitter.setParticleLifetime(sf::seconds(2.f));
-		m_emitter.setParticlePosition(thor::Distributions::circle(sf::Vector2f(300.f, 200.f), 50.f));
-		m_emitter.setParticleScale(sf::Vector2f(0.05f, 0.05f));
-		m_particleSystem.addEmitter(m_emitter);
+		m_particleSystem.addEmitter(thor::refEmitter(m_emitter));
 	}
 
 	Application::~Application()
@@ -47,16 +41,44 @@ namespace px
 	void Application::update(sf::Time dt)
 	{
 		ImGui::SFML::Update(m_window, dt);
+		updateParticles(dt);
+	}
+
+	void Application::updateParticles(sf::Time dt)
+	{
+		// TODO: Set texture on choice here?
+
+		// Update emitter
+		// TODO: Add conditional expressions when circular position is marked for instance
+		m_emitter.setEmissionRate(m_particle.nrOfParticles);
+		m_emitter.setParticleLifetime(sf::seconds(m_particle.lifetime));
+		m_emitter.setParticleScale(m_particle.scale);
+		m_emitter.setParticleRotation(m_particle.rotation);
+		m_emitter.setParticlePosition(thor::Distributions::circle(sf::Vector2f(300.f, 200.f), 50.f)); // Emit as circle
 		m_particleSystem.update(dt);
 	}
 
 	void Application::updateGUI()
 	{
-		ImGui::Begin("Hello, world!");
-		if (ImGui::Button("Look at this pretty button"))
+		static int floatPrecision = 3;
+		const ImGuiWindowFlags flags = ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize;
+		ImGui::Begin("Particle Editor", NULL, ImVec2(0, 0), 1.0f, flags);
+		
+		ImGui::SetNextTreeNodeOpen(true, 2);
+		if (ImGui::CollapsingHeader("Properties"))
 		{
-			printf("Hello\n");
+			ImGui::Spacing();
+			ImGui::InputFloat("Particles", &m_particle.nrOfParticles, 1.f);
+			ImGui::InputFloat2("Position", &m_particle.position.x, floatPrecision);
+			ImGui::Spacing();
+			ImGui::InputFloat2("Scale", &m_particle.scale.x, floatPrecision);
+			ImGui::Spacing();
+			ImGui::InputFloat("Rotation", &m_particle.rotation, 1.f, 0.f, floatPrecision);
+			ImGui::Spacing();
+			ImGui::InputFloat("Lifetime", &m_particle.lifetime, 0.1f, 0.f, floatPrecision);
 		}
+		ImGui::Spacing();
+
 		ImGui::End();
 	}
 
@@ -76,7 +98,7 @@ namespace px
 		{
 			pollEvents();
 			update(clock.restart());
-			//updateGUI();
+			updateGUI();
 			render();
 		}
 	}
